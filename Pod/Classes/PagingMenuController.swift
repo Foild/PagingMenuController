7aa823fc11622519d50575d6a2921f9f724987c8
bevbomb <grantlbevan@icloud.com>
2015-09-08 15:01:44 +1000

    Made current page index public

--- Pod/Classes/PagingMenuController.swift
+++ Pod/Classes/PagingMenuController.swift
@@ -26,7 +26,7 @@ public class PagingMenuController: UIViewController, UIScrollViewDelegate {
             options.menuItemCount = newValue.count
         }
     }
-    var currentPage: Int = 0
+    public var currentPage: Int = 0
     private var currentViewController: UIViewController!
     private var menuItemTitles: [String] {
         get {
